d743ab5b379a7370b9af822c8448da82175836f4
soumyagsr <34036848+soumyagsr@users.noreply.github.com>
2019-05-04 13:59:40 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,15 @@
-
 variable "region" {
   default = "us-west1"
 }
-variable "region_zone" {
+variable "zone" {
   default = "us-west1-a"
 }
 variable "project_name" {
-  default = "sgtest-229423"
+  default = "XXXX"
   description = "The ID of the Google Cloud project"
 }
-/*
-This example uses the default account credentials but you can also explicitly associate a new service account created using the console: IAM & Admin -> Service Accounts
+# Associate the service account created using the console: IAM & Admin -> Service Accounts
 variable "credentials_file_path" {
   description = "Path to the JSON file used to describe your account credentials"
-  default     = "~/.gcloud/Terraform.json"
+  default     = "~/.gcloud/tfkey.json"
 }
-*/

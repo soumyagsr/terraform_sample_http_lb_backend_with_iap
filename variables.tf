0fcf173d46aa9fc4c48b0b00a30836e3b381a118
soumyagsr <34036848+soumyagsr@users.noreply.github.com>
2019-05-05 06:56:19 -0700

    Added comments

--- variables.tf
+++ variables.tf
@@ -11,5 +11,5 @@ variable "project_name" {
 # Associate the service account created using the console: IAM & Admin -> Service Accounts
 variable "credentials_file_path" {
   description = "Path to the JSON file used to describe your account credentials"
-  default     = "~/.gcloud/tfkey.json"
+  default     = "~/tfkey.json"     # Put the .json key file in your home directory or specify the path if you created subdirs
 }
